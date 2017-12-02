12719af096ff0143dc5034f4a6cc0a49d5ca38d0
Francesco Levanti <francesco@levanti.net>
2017-12-02 15:15:31 +0000

    artisan command to run custom script and have named arguments

---
+++ app/Console/Commands/scr.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class scr extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'scr {filename : php file to run} {args?* : optional arguments}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Run custom script in a bootstrapped environment';
+
+    /**
+     * The string used to split key/value for arguments
+     *
+     * @var string
+     */
+    protected $arguments_delimiter = '=';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+      $args = $this->getSplittedArguments();
+      $this->line( "So you want to run " . $this->argument('filename') . " with these arguments!!!");
+      $this->line(print_r($args,true));
+      
+    }
+
+  /**
+   * Return arguments splitted using defined delimiter
+   *
+   * If delimiter is found array element is key[delimiter]value
+   * If delimiter is not found array element is argument=true
+   *
+   * only first delimiter found is used to split argument string
+   *
+   * @return array
+   */
+    protected function getSplittedArguments() {
+      $args = [];
+      foreach ($this->argument('args') as $argument) {
+        $ret = explode($this->arguments_delimiter, $argument, 2);
+        $args[$ret[0]] = $ret[1] ?? true;
+      }
+
+      return $args;
+
+    }
+
+}
